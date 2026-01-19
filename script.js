0b38ca1f87a20dadb8502dff295a38b53fc4c9ac
JorgeGithub21 <249790983+jorgems-dev@users.noreply.github.com>
2026-01-19 22:37:24 +0100

    Cambios y pruebas

--- script.js
+++ script.js
@@ -5,33 +5,6 @@ function toggleMenu() {
     icon.classList.toggle("open")
 }
 
-document.querySelectorAll('.nav-links a, .menu-links a, .btn-color-2').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    // Determinar el id de destino
-    let targetId = '';
-    if (this.classList.contains('btn-color-2')) {
-      targetId = 'contacto';
-    } else {
-      targetId = this.getAttribute('href').substring(1);
-    }
-
-    const targetEl = document.getElementById(targetId);
-    if (targetEl) {
-      targetEl.scrollIntoView({ behavior: 'smooth' });
-    }
-
-    // Cerrar menú desplegable si está abierto
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".icono-desplegable");
-    if (menu.classList.contains("open")) {
-      menu.classList.remove("open");
-      icon.classList.remove("open");
-    }
-  });
-});
-
 function abrirCV() {
     window.open("recursos/CVMihaiSilePavelAsiminei2026.pdf", "_blank")
 }
